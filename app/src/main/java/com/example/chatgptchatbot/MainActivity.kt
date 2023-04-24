6d66ac8a56010d7a4408e3d4456f33aaf062513a
frederikhendrix <rikhendrix@live.nl>
2023-04-24 10:21:39 +0200

    Takes Camera and displays the objects located on the camera screen

--- app/src/main/java/com/example/chatgptchatbot/MainActivity.kt
+++ app/src/main/java/com/example/chatgptchatbot/MainActivity.kt
@@ -1,66 +1,159 @@
 package com.example.chatgptchatbot
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.os.Bundle
-import android.provider.MediaStore
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
-import java.io.ByteArrayOutputStream
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.example.chatgptchatbot.ml.SsdMobilenetV11Metadata1
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
 
 class MainActivity : AppCompatActivity() {
-    // Define the button and imageview type variable
-    private lateinit var cameraOpenId: Button
-    lateinit var clickImageId: ImageView
+
+    lateinit var labels:List<String>
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED)
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap:Bitmap
+    lateinit var imageView: ImageView
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var model:SsdMobilenetV11Metadata1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        get_permission()
 
-        // By ID we can get each component which id is assigned in XML file get Buttons and imageview.
-        cameraOpenId = findViewById(R.id.camera_button)
-        clickImageId = findViewById(R.id.click_image)
+        labels = FileUtil.loadLabels(this, "labels.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(300, 300, ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = SsdMobilenetV11Metadata1.newInstance(this)
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
 
-        // Camera_open button is for open the camera and add the setOnClickListener in this button
-        cameraOpenId.setOnClickListener(View.OnClickListener { v: View? ->
-            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            // Start the activity with camera_intent, and request pic id
-            startActivityForResult(cameraIntent, pic_id)
-        })
+        imageView = findViewById(R.id.imageView)
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object:TextureView.SurfaceTextureListener{
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                open_camera()
+            }
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+                val outputs = model.process(image)
+                val locations = outputs.locationsAsTensorBuffer.floatArray
+                val classes = outputs.classesAsTensorBuffer.floatArray
+                val scores = outputs.scoresAsTensorBuffer.floatArray
+                val numberOfDetections = outputs.numberOfDetectionsAsTensorBuffer.floatArray
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+
+                val h = mutable.height
+                val w = mutable.width
+                paint.textSize = h/15f
+                paint.strokeWidth = h/85f
+                var x = 0
+                scores.forEachIndexed { index, fl ->
+                    x = index
+                    x *= 4
+                    if(fl > 0.5){
+                        paint.setColor(colors.get(index))
+                        paint.style = Paint.Style.STROKE
+                        canvas.drawRect(RectF(locations.get(x+1)*w, locations.get(x)*h, locations.get(x+3)*w, locations.get(x+2)*h), paint)
+                        paint.style = Paint.Style.FILL
+                        canvas.drawText(labels.get(classes.get(index).toInt())+" "+fl.toString(), locations.get(x+1)*w, locations.get(x)*h, paint)
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
+
+
+            }
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
     }
 
-    // This method will help to retrieve the image
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // Match the request 'pic id with requestCode
-        if (requestCode == pic_id) {
-            // BitMap is data structure of image file which store the image in memory
-            val photo = data!!.extras!!["data"] as Bitmap?
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
 
-            // Convert the Bitmap to a byte array
-            val stream = ByteArrayOutputStream()
-            photo?.compress(Bitmap.CompressFormat.PNG, 100, stream)
-            val byteArray = stream.toByteArray()
+    @SuppressLint("MissingPermission")
+    fun open_camera(){
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object:CameraDevice.StateCallback(){
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
 
-            // Load the object detection model using KotlinDL
-            val model = ObjectDetectionModel()
+                var surfaceTexture = textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
 
-            // Feed the image into the object detection model and obtain the bounding boxes and labels for the detected objects
-            val results = model.detectObjects(byteArray)
-            val boundingBoxes = results.boundingBoxes
-            val labels = results.labels
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
 
-            // Display the image with the bounding boxes and labels overlaid on top
-            val overlay = drawBoundingBoxes(photo!!, boundingBoxes, labels)
-            clickImageId.setImageBitmap(overlay)
+                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+                    }
+                }, handler)
+            }
 
+            override fun onDisconnected(p0: CameraDevice) {
+
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+
+            }
+        }, handler)
+    }
+
+    fun get_permission(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
         }
     }
-
-    companion object {
-        // Define the pic id
-        private const val pic_id = 123
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+            get_permission()
+        }
     }
 }
